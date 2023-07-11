976c9e91ad044263e64d6a63be7797356838f48a
Damian Wileński <wilenskid.dev@gmail.com>
2023-07-11 23:30:53 +0200

    v0.0.11

--- src/lib/error-listener/types/error-listeners-param.ts
+++
@@ -1,7 +0,0 @@
-import { LexingErrorListener } from '$workers/sparql/lexing-error-listener';
-import { ParsingErrorListener } from '$workers/sparql/parsing-error-listener';
-
-export interface ErrorListenersParam {
-  readonly lexingErrorListener?: LexingErrorListener;
-  readonly parsingErrorListener?: ParsingErrorListener;
-}
